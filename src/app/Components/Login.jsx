8d30cf244e87d0ac4274ba62789a60611de659f8
Subby575 <114326247+Subby575@users.noreply.github.com>
2023-09-21 19:30:21 +0530

    Update Login.jsx

--- src/app/Components/Login.jsx
+++ src/app/Components/Login.jsx
@@ -82,7 +82,7 @@ export default function Login({para,path}) {
             <path d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
           </svg>
         </span>
-        <span class="ml-2">You don't have an account?</span>
+        <span class="ml-2">You donot have an account?</span>
       </a>
     </div>
   </div>
