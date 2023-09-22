77dc1c1701e0c2f690f80f4e580f8ca3b817588c
Subby575 <114326247+Subby575@users.noreply.github.com>
2023-09-22 17:43:17 +0530

    Updated

--- src/app/Components/Ann.jsx
+++ src/app/Components/Ann.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 export default function Ann({ params }) {
     return (
         <>
-{/*             {
+            {
                 params.map((e) => (
-                    <div class="bg-white py-6 sm:py-8 lg:py-12">
+                    <>
+                    <div key={e.id} class="bg-white py-6 sm:py-8 lg:py-12">
                         <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
                             <div class="flex flex-col items-center justify-between gap-4 rounded-lg bg-gray-100 p-4 sm:flex-row md:p-8">
                                 <div>
@@ -17,9 +18,10 @@ export default function Ann({ params }) {
 
                         </div>
                     </div>
+                    </>
                 ))
-            } */}
-        <div>Announcements</div>
+            }
         </>
     )
 }
+
