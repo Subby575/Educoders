99efc324db754526e241fff1da2507792e874c04
Subby575 <114326247+Subby575@users.noreply.github.com>
2023-09-22 17:03:16 +0530

    Update Ann.jsx

--- src/app/Components/Ann.jsx
+++ src/app/Components/Ann.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Ann({ params }) {
     return (
         <>
-            {
+{/*             {
                 params.map((e) => (
                     <div class="bg-white py-6 sm:py-8 lg:py-12">
                         <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
@@ -18,7 +18,8 @@ export default function Ann({ params }) {
                         </div>
                     </div>
                 ))
-            }
+            } */}
+        <div>Announcements</div>
         </>
     )
 }
